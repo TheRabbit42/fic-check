27b1d8c50cd75f961e8e4678fd54fc3259d0dc05
Dan Jordan <daniel.ross.jordan@gmail.com>
2025-07-10 23:48:03 -0400

    Moving css

--- src/App.vue
+++ src/App.vue
@@ -265,23 +265,3 @@ function stripHtml(text) {
     <div v-html="renderedInput" class="rendered"></div>
   </div>
 </template>
-
-<style scoped>
-header {
-  line-height: 1.5;
-}
-
-@media (min-width: 1024px) {
-  header {
-    display: flex;
-    place-items: center;
-    padding-right: calc(var(--section-gap) / 2);
-  }
-
-  header .wrapper {
-    display: flex;
-    place-items: flex-start;
-    flex-wrap: wrap;
-  }
-}
-</style>
