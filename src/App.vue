e6dec2f6fae4a0caed176ff83daea7ebeeca2a53
Dan Jordan <daniel.ross.jordan@gmail.com>
2025-07-10 23:48:16 -0400

    Detecting carriage returns

--- src/App.vue
+++ src/App.vue
@@ -9,6 +9,7 @@ const wordCount = computed(() => {
   let s = input.value
   s = s.replace(/(^\s*)|(\s*$)/gi, ""); // exclude start and end white-space
   s = s.replace(/(\u00a9|\u00ae|[\u2000-\u3300]|\ud83c[\ud000-\udfff]|\ud83d[\ud000-\udfff]|\ud83e[\ud000-\udfff])/g, ' ');
+  s = s.replace(/(\u000d)/g, ' '); // Carriage returns
   s = s.replace(/\n /gi, '\n'); // exclude newline with a start spacing
   s = s.replace(/<\/?\w*\/?>/gi, ' ');
   s = s.replace(/(?<=\w)—(?=\w)/gi, ' '); // split em-dashes
