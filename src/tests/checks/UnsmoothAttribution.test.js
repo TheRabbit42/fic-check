74d281228cbf473effa05b6c1c1b7328cca5a07a
Dan Jordan <daniel.ross.jordan@gmail.com>
2026-08-18 02:45:56 -0400

    Improving unsmooth attribute tests

--- src/tests/checks/UnsmoothAttribution.test.js
+++ src/tests/checks/UnsmoothAttribution.test.js
@@ -1,41 +1,25 @@
 import { describe, it, expect } from "vitest";
 import { UnsmoothAttribution } from "../../checks/UnsmoothAttribution.js";
+import { speakingVerbs } from "../../data/speakingVerbs.js";
 
 describe("UnsmoothAttribution", () => {
     const check = new UnsmoothAttribution();
 
-    it(`detects ambiguous attribution`, () => {
-        const text = `"Why?" Marinette said.`;
-        expect(check.isInParagraph(text)).toBe(true);
+    speakingVerbs.forEach((verb) => {
+        ['?', '!'].forEach((quoteEnder) => {
+            it(`detects with ${quoteEnder}, name, ${verb}`, () => {
+                const text = `"Gah${quoteEnder}" Marinette ${verb}.`;
+                expect(check.isInParagraph(text)).toBe(true);
+            });
+        });
     });
 
-    it(`detects ambiguous attribution`, () => {
-        const text = `"No!" Marinette said.`;
-        expect(check.isInParagraph(text)).toBe(true);
-    });
-
-    it(`detects ambiguous attribution`, () => {
-        const text = `"No!" Marinette shouted.`;
-        expect(check.isInParagraph(text)).toBe(true);
-    });
-
-    it(`ignores said after question mark`, () => {
-        const text = `"Why?" said Marinette.`;
-        expect(check.isInParagraph(text)).toBe(false);
-    });
-
-    it(`ignores asked after question mark`, () => {
-        const text = `"Why?" asked Marinette.`;
-        expect(check.isInParagraph(text)).toBe(false);
-    });
-
-    it(`ignores said after question mark`, () => {
-        const text = `"Yes!" said Marinette.`;
-        expect(check.isInParagraph(text)).toBe(false);
-    });
-
-    it(`ignores asked after question mark`, () => {
-        const text = `"Yes?" exclaimed Marinette.`;
-        expect(check.isInParagraph(text)).toBe(false);
+    speakingVerbs.forEach((verb) => {
+        ['?', '!'].forEach((quoteEnder) => {
+            it(`ignores with ${quoteEnder}, ${verb}, name`, () => {
+                const text = `"Gah${quoteEnder}" ${verb} Marinette.`;
+                expect(check.isInParagraph(text)).toBe(false);
+            });
+        });
     });
 });
